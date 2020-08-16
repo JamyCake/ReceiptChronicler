ce4fefb3f89d82aa87ba829a456a4f8d6630a22f
Igor Kim <jamycake@hotmail.com>
2020-08-16 11:06:17 +0300

    refactor class SberbankReceiptNameProvider

--- src/main/java/com/jamycake/chronicler/subscriber/SberbankReceiptNameProvider.java
+++ src/main/java/com/jamycake/chronicler/subscriber/SberbankReceiptNameProvider.java
@@ -11,17 +11,31 @@ public class SberbankReceiptNameProvider {
         String [] receiptStrings = receiptContent.split("[\n\r]+");
 
         if (receiptStrings[5].contains(PAYMENT)){
-            String name = receiptStrings[4] + receiptStrings[12];
-            return makeReplaces(name);
+            return getPaymentReceiptName(receiptStrings);
         }
         if (receiptStrings[5].contains(RECONCILIATION_OF_RESULTS)){
-            String name = RECONCILIATION_OF_RESULTS + " " +
-                    receiptStrings[17] + "-" +
-                    receiptStrings [16];
-            return makeReplaces(name);
+            return getReconciliationReceiptName(receiptStrings);
         } else {
             return UNKNOWN_RECEIPT_TYPE;
         }
+    }
+
+    private String getPaymentReceiptName(String[] receiptStrings) {
+        final int RECEIPT_INDICATOR_INDEX = 4;
+        final int RECEIPT_SUM_INDEX = 12;
+
+        String name = receiptStrings[RECEIPT_INDICATOR_INDEX] + receiptStrings[RECEIPT_SUM_INDEX];
+        return makeReplaces(name);
+    }
+
+    private String getReconciliationReceiptName(String[] receiptStrings) {
+        final int TOTAL_OPERATION_INDEX = 16;
+        final int TOTAL_SUM_INDEX = 15;
+
+        String name = RECONCILIATION_OF_RESULTS + " " +
+                receiptStrings[TOTAL_OPERATION_INDEX] + "-" +
+                receiptStrings[TOTAL_SUM_INDEX];
+        return makeReplaces(name);
     }
 
     private String makeReplaces(String str){
